4078ecaaa1c3f2d560641a8b5ee3dcd66787c78f
GaelicGrime <will.angus.blaylock@gmail.com>
2021-12-10 21:17:26 -0700

    Updated makeKML_C.py.

--- IARUGridTools/imageMaker.py
+++ IARUGridTools/imageMaker.py
@@ -2,31 +2,14 @@
 
 
 import maidenhead_C
+from makeKML_C import makeKML_C
 MH = maidenhead_C.maidenhead()
 
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00aa00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00bb00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00cc00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00dd00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00ee00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00ff00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00gg00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00hh00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00ii00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00jj00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00kk00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00ll00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00mm00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00nn00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00oo00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00pp00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00qq00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00rr00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00ss00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00tt00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00uu00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00vv00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00ww00aa", center_=False)}""")
-print(f"""{MH.maidenheadTolatlon(maidenhead_="DN00aa00xx00aa", center_=False)}""")
+
+with makeKML_C(fileName_="output.kml") as KML:
+	KML.documentTop()
+	KML.placemark()
+	KML.documentEnd()
+	print(KML.STR_TO_RETURN)
 
 #
