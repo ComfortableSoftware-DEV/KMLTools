90faceba576428fc3de52f365ad8be73073be154
GaelicGrime <will.angus.blaylock@gmail.com>
2021-12-19 19:09:47 -0700

    Updated templates.txtkey-goofing .

--- IARUGridTools/maidenhead_C.py
+++ IARUGridTools/maidenhead_C.py
@@ -195,7 +195,7 @@ class maidenhead_C(object):
   ):
     """
 Input is:
-    chrs_		How many characters of maidenhead to return, must be even from 2 to 12.
+    chrs_    How many characters of maidenhead to return, must be even from 2 to 12.
     lat_=  40.687216, lon_=-111.927653, chrs_=12
          2 = DN
          4 = DN40
@@ -213,7 +213,7 @@ Output is a Maidenhead string with specified character length.
     _latIndex_ = (_lat_ / self._ONE_TWO_LAT_INC_)
     _lonIndex_ = (_lon_ / self._ONE_TWO_LON_INC_)
     _strToRtn_ += f"""{self._ONE_TWO_CHAR_LIST_[int(_lonIndex_)]}{self._ONE_TWO_CHAR_LIST_[int(_latIndex_)]}"""
-# 		print(f"""first
+#     print(f"""first
 # lat_ {lat_}   _lat_ {_lat_}
 # lon_ {lon_}   _lon_ {_lon_}
 # _latIndex_ {_latIndex_}
@@ -232,7 +232,7 @@ Output is a Maidenhead string with specified character length.
     _latIndex_ = (_lat_ / self._THREE_FOUR_LAT_INC_)
     _lonIndex_ = (_lon_ / self._THREE_FOUR_LON_INC_)
     _strToRtn_ += f"""{self._THREE_FOUR_CHAR_LIST_[int(_lonIndex_)]}{self._THREE_FOUR_CHAR_LIST_[int(_latIndex_)]}"""
-# 		print(f"""second
+#     print(f"""second
 # lat_ {lat_}   _lat_ {_lat_}
 # lon_ {lon_}   _lon_ {_lon_}
 # _latIndex_ {_latIndex_}
@@ -250,7 +250,7 @@ Output is a Maidenhead string with specified character length.
     _latIndex_ = (_lat_ / self._FIVE_SIX_LAT_INC_)
     _lonIndex_ = (_lon_ / self._FIVE_SIX_LON_INC_)
     _strToRtn_ += f"""{self._FIVE_SIX_CHAR_LIST_[int(_lonIndex_)]}{self._FIVE_SIX_CHAR_LIST_[int(_latIndex_)]}"""
-# 		print(f"""third
+#     print(f"""third
 # lat_ {lat_}   _lat_ {_lat_}
 # lon_ {lon_}   _lon_ {_lon_}
 # _latIndex_ {_latIndex_}
@@ -268,7 +268,7 @@ Output is a Maidenhead string with specified character length.
     _latIndex_ = (_lat_ / self._SEVEN_EIGHT_LAT_INC_)
     _lonIndex_ = (_lon_ / self._SEVEN_EIGHT_LON_INC_)
     _strToRtn_ += f"""{self._SEVEN_EIGHT_CHAR_LIST_[int(_lonIndex_)]}{self._SEVEN_EIGHT_CHAR_LIST_[int(_latIndex_)]}"""
-# 		print(f"""fourth
+#     print(f"""fourth
 # lat_ {lat_}   _lat_ {_lat_}
 # lon_ {lon_}   _lon_ {_lon_}
 # _latIndex_ {_latIndex_}
@@ -286,7 +286,7 @@ Output is a Maidenhead string with specified character length.
     _latIndex_ = (_lat_ / self._NINE_TEN_LAT_INC_)
     _lonIndex_ = (_lon_ / self._NINE_TEN_LON_INC_)
     _strToRtn_ += f"""{self._NINE_TEN_CHAR_LIST_[int(_lonIndex_)]}{self._NINE_TEN_CHAR_LIST_[int(_latIndex_)]}"""
-# 		print(f"""fourth
+#     print(f"""fourth
 # lat_ {lat_}   _lat_ {_lat_}
 # lon_ {lon_}   _lon_ {_lon_}
 # _latIndex_ {_latIndex_}
@@ -304,7 +304,7 @@ Output is a Maidenhead string with specified character length.
     _latIndex_ = (_lat_ / self._ELEVEN_TWELVE_LAT_INC_)
     _lonIndex_ = (_lon_ / self._ELEVEN_TWELVE_LON_INC_)
     _strToRtn_ += f"""{self._ELEVEN_TWELVE_CHAR_LIST_[int(_lonIndex_)]}{self._ELEVEN_TWELVE_CHAR_LIST_[int(_latIndex_)]}"""
-# 		print(f"""fifth
+#     print(f"""fifth
 # lat_ {lat_}   _lat_ {_lat_}
 # lon_ {lon_}   _lon_ {_lon_}
 # _latIndex_ {_latIndex_}
